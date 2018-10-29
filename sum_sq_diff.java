d56c9ec1383263176e277dc76940cfa809ee63ae
sumukhmlohit <43979295+sumukhmlohit@users.noreply.github.com>
2018-10-29 23:06:35 +0530

    Add files via upload

---
+++ sum_sq_diff.java
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        long n;
+        cin >> n;
+        long sum,sum_sq,sum2;
+        sum=n*(n+1)/2;
+        sum2=sum*sum;
+        sum_sq=n*(n+1)*(2*n+1)/6;
+        cout<<sum2-sum_sq<<endl;
+    }
+    return 0;
+}
